2a8476fd5662c9974e7b4fcb2c73ccb7806422e1
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:27:44 +0530

    Add files via upload

---
+++ program9string.java
@@ -0,0 +1,12 @@
+package myjava1;
+
+public class program9string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(7+7);
+		System.out.println(7+7.0);
+		System.out.println(7+'7');
+	}
+
+}
