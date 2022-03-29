91315d693cf1a74c0f2f668e1557cee76e677df6
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:36:11 +0530

    Add files via upload

---
+++ program26cltofh.java
@@ -0,0 +1,15 @@
+package myjava1;
+
+import java.util.Scanner;
+
+public class program26cltofh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc=new Scanner(System.in);
+	        double celsius=sc.nextDouble();
+	        System.out.println(celsius*1.8+32+" F");
+	        sc.close();
+	}
+
+}
