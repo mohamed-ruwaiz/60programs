c3a262d58a3e98edd204f966e908a2d9c1bf3e22
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:26:47 +0530

    Add files via upload

---
+++ program5byteshort.java
@@ -0,0 +1,10 @@
+package myjava1;
+
+public class program5byteshort {
+      public static void main(String []args)
+      {
+    	  short shortNumber=150;
+    	  byte byteNumber=(byte) shortNumber;
+    	  System.out.println(byteNumber);
+      }
+}
