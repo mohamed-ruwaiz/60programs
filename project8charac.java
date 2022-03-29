d6c965d382e79742db80b00f2f9e1b07d31ca462
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:41:05 +0530

    Add files via upload

---
+++ project8charac.java
@@ -0,0 +1,12 @@
+package myjava1;
+
+public class project8charac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int number55 = 55;
+      char character55 = ( char ) number55;
+      System.out.println(number55);
+	}
+
+}
