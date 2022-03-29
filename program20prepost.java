b6f3ac1cacfe107402bd781a5fd73b80326efd7f
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:34:04 +0530

    Add files via upload

---
+++ program20prepost.java
@@ -0,0 +1,15 @@
+package myjava1;
+
+public class program20prepost {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int preInc,postInc,preDec,postDec;
+    preInc=postInc=preDec=postDec=10;
+    System.out.println(++preInc);
+    System.out.println(postInc++);
+    System.out.println(--preDec);
+    System.out.println(postDec--);
+	}
+
+}
