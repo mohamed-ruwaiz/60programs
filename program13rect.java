65915ec6ea131d1d66bd263cb0f0cc3e881050fb
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:31:31 +0530

    Add files via upload

---
+++ program13rect.java
@@ -0,0 +1,14 @@
+package myjava1;
+import java.util.*;
+public class program13rect {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+    	float len = sc.nextInt();
+    	float breath =sc.nextInt();
+    	System.out.println(len*breath);
+   sc.close();
+	}
+
+}
