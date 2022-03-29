a01346244b29d5eb56705645a28fbd14badf1a3f
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:37:25 +0530

    Add files via upload

---
+++ program32.java
@@ -0,0 +1,14 @@
+package myjava1;
+
+
+public class program32 {
+      public static void main(String[] args) {
+    	  int num=7;
+    	  if (num %2==0) {
+    		  System.out.println("entered num is even");
+    	  }
+    	  else {
+    		  System.out.println("enterted num is odd");
+    	  }
+      }
+}
