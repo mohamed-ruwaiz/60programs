94979d2eec3ee1c3c3ad852c8e57e9ed642e1d52
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:35:22 +0530

    Add files via upload

---
+++ program24.java
@@ -0,0 +1,13 @@
+package myjava1;
+
+public class program24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+     int y=10;
+     int z=(++y*(y++ +5));
+    System.out.println(z);
+	}
+
+}
