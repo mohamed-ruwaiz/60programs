013dd0b8281597049c1263929c99dad026121891
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:36:30 +0530

    Add files via upload

---
+++ program26kmtomi.java
@@ -0,0 +1,15 @@
+package myjava1;
+
+import java.util.Scanner;
+
+public class program26kmtomi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+        double km=sc.nextDouble();
+        System.out.println(km*0.621371+" miles");
+    sc.close();
+	}
+
+}
