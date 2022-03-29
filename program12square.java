60d13e9c76a2a77b54af336b1fe4ef438103f8e1
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:31:08 +0530

    Add files via upload

---
+++ program12square.java
@@ -0,0 +1,14 @@
+package myjava1;
+import java.util.*;
+public class program12square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("enter the value : ");
+    Scanner sc = new Scanner(System.in);
+    float side =sc.nextFloat();
+    System.out.println(side*side);
+    sc.close();
+	}
+
+}
