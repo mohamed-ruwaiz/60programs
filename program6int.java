a5ac3993a351970acb23cd90a1093cef6e6070a5
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:27:02 +0530

    Add files via upload

---
+++ program6int.java
@@ -0,0 +1,13 @@
+package myjava1;
+
+public class program6int {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    	 float floatNumber =150.9f;
+	    	 int intNumber = (int)floatNumber;
+	    	 System.out.println(intNumber);
+	     
+	}
+
+}
