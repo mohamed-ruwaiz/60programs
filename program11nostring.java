1aea50ba275c886e250eab54c8a55ad2f0386e5a
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:30:50 +0530

    Add files via upload

---
+++ program11nostring.java
@@ -0,0 +1,11 @@
+package myjava1;
+
+public class program11nostring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     float f =7.50f;
+     System.out.printf("%.2f",f);
+	}
+
+}
