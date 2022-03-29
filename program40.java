af044e85ad2cf89caf6c2f23460b2175ceac115b
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:38:20 +0530

    Add files via upload

---
+++ program40.java
@@ -0,0 +1,15 @@
+package myjava1;
+
+public class program40 {
+
+	public static void main(String[] args)
+	{
+		int N = 11;
+
+		for (int i = 1; i <= 10; i++) {
+
+			System.out.println(i + " * " + N + " = "
+							+ N * i);
+		}
+	}
+}
