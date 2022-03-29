220771914fdcd321d5621ed8ca30d5e923a44567
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:27:18 +0530

    Add files via upload

---
+++ program7int.java
@@ -0,0 +1,12 @@
+package myjava1;
+
+public class program7int {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 char character7 ='7';
+		  int number7 =(int) character7;
+		  System.out.println(number7);
+	}
+
+}
