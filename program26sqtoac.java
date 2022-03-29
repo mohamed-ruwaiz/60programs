40b2ef4f3f0a642904117b748e64f9810399d89b
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:36:48 +0530

    Add files via upload

---
+++ program26sqtoac.java
@@ -0,0 +1,15 @@
+package myjava1;
+
+import java.util.Scanner;
+
+public class program26sqtoac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Scanner sc=new Scanner(System.in);
+	        double sqft=sc.nextDouble();
+	        System.out.println(sqft*2.296*Math.pow(10,-5)+" Acres");
+sc.close();
+	}
+
+}
