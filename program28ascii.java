9b3feb80bd73498a222bdf9b1244491ecf7869af
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:37:06 +0530

    Add files via upload

---
+++ program28ascii.java
@@ -0,0 +1,14 @@
+package myjava1;
+import java.util.*;
+public class program28ascii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc =new Scanner(System.in);
+    System.out.println("enter the character : ");
+    char alphabet =sc.next().charAt(0);
+    System.out.println((int)alphabet);
+    sc.close();
+	}
+
+}
