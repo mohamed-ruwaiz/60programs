d0ce9912605dd3d6c05ddac3ebb95c7017b84252
mohamed-ruwaiz <98874861+mohamed-ruwaiz@users.noreply.github.com>
2022-03-29 23:42:54 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,11 @@
+package myjava1;
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a=4,b=8;
+   System.out.println("bitwise and : "+(a&b));
+	}
+
+}
